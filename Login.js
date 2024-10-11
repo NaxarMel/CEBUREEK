b6c5f4bdc52dd1ee18f8d2ded3a63ef0c1ca92d3
NaxarMel <133958584+NaxarMel@users.noreply.github.com>
2024-10-11 12:09:39 +0300

    Delete Login.js

--- Login.js
+++
@@ -1,9 +0,0 @@
-window.onload = () =>
-{
-    const form = document.querySelector('.box-form')
-
-    document.querySelector('.box-form h2').onclick = () => form.classList.add('box-form-open')
-
-    document.querySelector('.close').onclick = () => form.classList.remove('box-form-open')
-    
-}
