dec50937e4d4de07425fa7a04cfd08f6bf69f808
NaxarMel <melnyk.n@nltu.lviv.ua>
2024-10-11 12:01:50 +0300

    Project C

---
+++ Login.js
@@ -0,0 +1,9 @@
+window.onload = () =>
+{
+    const form = document.querySelector('.box-form')
+
+    document.querySelector('.box-form h2').onclick = () => form.classList.add('box-form-open')
+
+    document.querySelector('.close').onclick = () => form.classList.remove('box-form-open')
+    
+}
